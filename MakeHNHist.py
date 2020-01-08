0be16521a3adf14d7637ecd7402d1d5888a1212c
Hooooon12 <theking461@snu.ac.kr>
2020-01-09 08:09:48 +0900

    MakeHNHist.py -> run in background

---
+++ MakeHNHist.py
@@ -0,0 +1,10 @@
+import os
+
+ptcuts = ["ptcut5","ptcut10","ptcut15","ptcut20","ptl10_ptj30","ptl20_ptj30"]
+channels = ["MuMu"]
+masses = ["1500","2000"]
+
+for ptcut in ptcuts:
+  for channel in channels:
+    for mass in masses:
+      os.system("bin/MakeHist.sh"+" "+"MG"+" "+"HeavyMajoranaNeutrino_SSDiLepton_Tchannel_NLO_{0}_{1}_M{2}".format(ptcut, channel, mass)+" "+"script/HNtype1.cc"+"&")
