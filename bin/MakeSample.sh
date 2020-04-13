b7a904fdbac8d85841e42a93b5ec97fd9d69b6d0
Hooooon12 <theking461@snu.ac.kr>
2020-04-14 07:31:49 +0900

    GT v20

--- bin/MakeSample.sh
+++ bin/MakeSample.sh
@@ -37,7 +37,7 @@ then
   mkdir -p $WORKING_DIR
   cd $WORKING_DIR
   echo "#!/bin/bash" > run_GS.sh
-  echo "cmsDriver.py MY/mg/python/${PROCESSNAME}.py --fileout file:${PROCESSNAME}_GS.root --mc --eventcontent RAWSIM,LHE --datatier GEN-SIM,LHE --conditions 102X_upgrade2018_realistic_v9 --beamspot Realistic25ns13TeVEarly2018Collision --step LHE,GEN,SIM --nThreads 8 --geometry DB:Extended --era Run2_2018 --python_filename ${PROCESSNAME}_GS_cfg.py -n $NEVENT" >> run_GS.sh #following RunIIFall18wmLHEGS campaign
+  echo "cmsDriver.py MY/mg/python/${PROCESSNAME}.py --fileout file:${PROCESSNAME}_GS.root --mc --eventcontent RAWSIM,LHE --datatier GEN-SIM,LHE --conditions 102X_upgrade2018_realistic_v20 --beamspot Realistic25ns13TeVEarly2018Collision --step LHE,GEN,SIM --nThreads 8 --geometry DB:Extended --era Run2_2018 --python_filename ${PROCESSNAME}_GS_cfg.py -n $NEVENT" >> run_GS.sh #following RunIIFall18wmLHEGS campaign
   chmod +x run_GS.sh
   if [[ $GENERATORTOOLS_USECONDOR = 1 ]]
   then
@@ -74,7 +74,7 @@ EOF
   echo "cp \"$GENERATORTOOLS_BASE/x509up_u\"* \"/tmp/x509up_u\"`id -u`" >> run_step1.sh
   echo "ls -hlt /tmp/" >> run_step1.sh
   echo "echo \"Making config file...\"" >> run_step1.sh
-  echo "time bash -c \"cmsDriver.py step1 --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 102X_upgrade2018_realistic_v15 --step DIGI,DATAMIX,L1,DIGI2RAW,HLT:@relval2018 --procModifiers premix_stage2 --nThreads 8 --geometry DB:Extended --datamix PreMix --era Run2_2018 --filein file:${PROCESSNAME}_GS.root --fileout file:${PROCESSNAME}_step1.root --pileup_input \"dbs:/Neutrino_E-10_gun/RunIISummer17PrePremix-PUAutumn18_102X_upgrade2018_realistic_v15-v1/GEN-SIM-DIGI-RAW\" --python_filename ${PROCESSNAME}_step1_cfg.py -n $NEVENT --no_exec\"" >> run_step1.sh #following RunIIAutumn18DRPremix campaign
+  echo "time bash -c \"cmsDriver.py step1 --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 102X_upgrade2018_realistic_v20 --step DIGI,DATAMIX,L1,DIGI2RAW,HLT:@relval2018 --procModifiers premix_stage2 --nThreads 8 --geometry DB:Extended --datamix PreMix --era Run2_2018 --filein file:${PROCESSNAME}_GS.root --fileout file:${PROCESSNAME}_step1.root --pileup_input \"dbs:/Neutrino_E-10_gun/RunIISummer17PrePremix-PUAutumn18_102X_upgrade2018_realistic_v15-v1/GEN-SIM-DIGI-RAW\" --python_filename ${PROCESSNAME}_step1_cfg.py -n $NEVENT --no_exec\"" >> run_step1.sh #following RunIIAutumn18DRPremix campaign
   echo "sed -i \"s/\/store/root:\/\/eoscms.cern.ch\/\/eos\/cms\/store/g\" ${PROCESSNAME}_step1_cfg.py" >> run_step1.sh
   echo "echo \"Now cmsRun...\"" >> run_step1.sh
   echo "cmsRun ${PROCESSNAME}_step1_cfg.py" >> run_step1.sh
@@ -110,7 +110,7 @@ EOF
 
   echo -n "Submitting step2 jobs"
   echo "#!/bin/bash" > run_step2.sh
-  echo "cmsDriver.py step2 --mc --eventcontent AODSIM --runUnscheduled --datatier AODSIM --conditions 102X_upgrade2018_realistic_v15 --step RAW2DIGI,L1Reco,RECO,RECOSIM,EI --procModifiers premix_stage2 --nThreads 8 --era Run2_2018 --filein file:${PROCESSNAME}_step1.root --fileout file:${PROCESSNAME}_step2.root --python_filename ${PROCESSNAME}_step2_cfg.py -n $NEVENT" >> run_step2.sh #following RunIIAutumn18DRPremix campaign
+  echo "cmsDriver.py step2 --mc --eventcontent AODSIM --runUnscheduled --datatier AODSIM --conditions 102X_upgrade2018_realistic_v20 --step RAW2DIGI,L1Reco,RECO,RECOSIM,EI --procModifiers premix_stage2 --nThreads 8 --era Run2_2018 --filein file:${PROCESSNAME}_step1.root --fileout file:${PROCESSNAME}_step2.root --python_filename ${PROCESSNAME}_step2_cfg.py -n $NEVENT" >> run_step2.sh #following RunIIAutumn18DRPremix campaign
   chmod +x run_step2.sh
   if [[ $GENERATORTOOLS_USECONDOR = 1 ]]
   then
@@ -143,7 +143,7 @@ EOF
 
   echo -n "Submitting MiniAOD jobs"
   echo "#!/bin/bash" > run_MiniAOD.sh
-  echo "cmsDriver.py step1 --mc --eventcontent MINIAODSIM --runUnscheduled --datatier MINIAODSIM --conditions 102X_upgrade2018_realistic_v15 --step PAT --nThreads 8 --geometry DB:Extended --era Run2_2018 --filein file:${PROCESSNAME}_step2.root --fileout file:${PROCESSNAME}_MiniAOD.root --python_filename ${PROCESSNAME}_MiniAOD_cfg.py -n $NEVENT" >> run_MiniAOD.sh #following RunIIAutumn18MiniAOD campaign
+  echo "cmsDriver.py step1 --mc --eventcontent MINIAODSIM --runUnscheduled --datatier MINIAODSIM --conditions 102X_upgrade2018_realistic_v20 --step PAT --nThreads 8 --geometry DB:Extended --era Run2_2018 --filein file:${PROCESSNAME}_step2.root --fileout file:${PROCESSNAME}_MiniAOD.root --python_filename ${PROCESSNAME}_MiniAOD_cfg.py -n $NEVENT" >> run_MiniAOD.sh #following RunIIAutumn18MiniAOD campaign
   chmod +x run_MiniAOD.sh
   if [[ $GENERATORTOOLS_USECONDOR = 1 ]]
   then
